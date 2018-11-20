a0af1d66edadd362389c84f2b370bbc47161d2e1
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-11-20 15:40:26 +0530

    QuestionDetailView in urls

--- quena/urls.py
+++ quena/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 app_name = "quena"
 urlpatterns = [
     path("", views.QuestionCreateView.as_view(), name="ask"),
+    path("question/<int:pk>/", views.QuestionDetailView.as_view(), name="question_detail"),
 ]
