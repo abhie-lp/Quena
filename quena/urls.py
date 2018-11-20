ae81f201d46135cdf6b5da3a3724f6fb73db71c9
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-11-20 16:38:52 +0530

    UpdateAnswerAcceptanceView

--- quena/urls.py
+++ quena/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.QuestionCreateView.as_view(), name="ask"),
     path("question/<int:pk>/", views.QuestionDetailView.as_view(), name="question_detail"),
     path("question/<int:pk>/answer/", views.AnswerCreateView.as_view(), name="answer_question"),
+    path("answer/<int:pk>/accept/", views.UpdateAnswerAcceptanceView.as_view(), name="update_answer_acceptance"),
 ]
