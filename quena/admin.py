d52b7c81c651b12a08134f54c9f7d97bf9be7960
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-11-20 16:21:35 +0530

    Configured admin page

--- quena/admin.py
+++ quena/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = "title", "user", "created",
+    list_filter = "created", "updated",
+    search_fields = "title",
+    inlines = AnswerInline,
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = "question", "user", "created", "accepted",
+    list_filter = "created", "updated",
+    list_editable = "accepted",
+    search_fields = "question", "answer",
