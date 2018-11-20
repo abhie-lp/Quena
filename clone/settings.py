a52a40ab601bbf0c86bf17f25309733edb4d6ed6
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-11-20 14:03:34 +0530

    include quena app in urls

--- clone/settings.py
+++ clone/settings.py
@@ -119,7 +119,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
 
 MARKDOWNIFY_STRIP = False
 MARKDOWNIFY_WHITELIST_TAGS = [
