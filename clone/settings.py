27f13ae3aa7ce9e88719adf07e9cdcb5421babb3
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-11-20 09:12:31 +0530

    Added quena to settings

--- clone/settings.py
+++ clone/settings.py
@@ -27,6 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += [
+    "quena.apps.QuenaConfig",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
