11fe8ad63a1e46466dab50bd0662b9f56b14a8d0
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-11-20 09:09:28 +0530

    Changed database to postgresql

--- clone/settings.py
+++ clone/settings.py
@@ -61,13 +61,20 @@ WSGI_APPLICATION = 'clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "stack_clone",
+        "USER": "at",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
