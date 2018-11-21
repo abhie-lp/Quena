08135f74e89ab7ef068d92822a0e7508cb61a6f3
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-11-21 13:46:12 +0530

    Register view and url

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ app_name = "user"
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", views.UserRegisterView.as_view(), name="register"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .forms import UserForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class UserRegisterView(CreateView):
+    form_class = UserForm
+    template_name = "user/register.html"
+    success_url = reverse_lazy("user:login")
