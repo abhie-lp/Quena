d5c7a2f17b347e5cc26f30bfca93af6bee7a5533
Abhishek Tiwari <abhie.lp@gmail.com>
2018-11-26 18:49:05 +0530

    SearchView

--- quena/urls.py
+++ quena/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("question/<int:pk>/answer/", views.AnswerCreateView.as_view(), name="answer_question"),
     path("answer/<int:pk>/accept/", views.UpdateAnswerAcceptanceView.as_view(), name="update_answer_acceptance"),
     path("daily_questions/<int:year>/<int:month>/<int:day>/", views.DailyQuestionListView.as_view(), name="daily_questions"),
+    path("question/search/", views.SearchView.as_view(), name="question_search"),
 ]
